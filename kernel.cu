31c94c3dc2f778d232ef348f754ee5bef8381526
p10tr13 <160889621+p10tr13@users.noreply.github.com>
2024-12-20 12:48:20 +0100

    Liczymy juz X na GPU

--- kernel.cu
+++ kernel.cu
@@ -5,10 +5,17 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <cstdlib>
+#include <string.h>
 
 #define S2 "sadsfa"
 
 #define S1 "dsags"
+
+#define ALPHLEN 26
+
+#define ALPHSTART 'a'
+
+#define ALPHEND 'z'
 
 // Struktura listy jednokierunkowej, dla opisywania operacji zmiany s1 na s2
 // Typ operacji
@@ -27,7 +34,7 @@ typedef struct Node
 	struct Node* next;
 } Node;
 
-cudaError_t addWithCuda(int* c, const int* a, const int* b, unsigned int size);
+cudaError_t LevenshteinGPU(char* s1, char* s2, const uint16_t s1Len, const uint16_t s2Len, uint16_t* D, uint16_t* X);
 
 __host__ uint16_t checkWord(char* s);
 
@@ -39,12 +46,16 @@ __host__ void CPULevenshtein(char* s1, uint16_t s1Len, char* s2, uint16_t s2Len,
 
 __host__ void PrintD(uint16_t* D, uint16_t height, uint16_t width, char* s1, char* s2);
 
+__host__ void PrintX(uint16_t* X, uint16_t height, uint16_t width, char* s2);
+
 __host__ Node* RetrievePath(uint16_t* D, uint16_t height, uint16_t width, char* s1, char* s2);
 
 __global__ void addKernel(int* c, const int* a, const int* b)
 {
 
 }
+
+__global__ void calculateX(uint16_t* X, char* s2, const uint16_t s2Len);
 
 // Operacje na liście jednokierunkowej, dla opisywania operacji zmiany s1 na s2
 Node* createNode(uint16_t ind, char letter, OperationType type);
@@ -81,11 +92,22 @@ int main(int argc, char* argv[])
 
 	CPULevenshtein(s1, s1Len, s2, s2Len, D_CPU);
 
-	PrintD(D_CPU, s1Len + 1, s2Len + 1, s1, s2);
+	//PrintD(D_CPU, s1Len + 1, s2Len + 1, s1, s2);
 
 	Node* result = RetrievePath(D_CPU, s1Len + 1, s2Len + 1, s1, s2);
 
 	printList(result);
+
+	uint16_t* D_GPU = (uint16_t*)malloc(sizeof(uint16_t) * (s1Len + 1) * (s2Len + 1));
+	uint16_t* X_GPU = (uint16_t*)malloc(sizeof(uint16_t) * ALPHSTART * (s2Len + 1));
+	cudaStatus = LevenshteinGPU(s1, s2, s1Len, s2Len, D_GPU, X_GPU);
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "LevenshteinGPU failed! %s\n", cudaGetErrorString(cudaStatus));
+		return 1;
+	}
+
+	PrintX(X_GPU, ALPHLEN, s2Len + 1, s2);
 
 	cudaStatus = cudaDeviceReset();
 	if (cudaStatus != cudaSuccess)
@@ -96,6 +118,8 @@ int main(int argc, char* argv[])
 
 	freeList(result);
 	free(D_CPU);
+	free(D_GPU);
+	free(X_GPU);
 	return 0;
 }
 
@@ -177,6 +201,30 @@ __host__ void PrintD(uint16_t* D, uint16_t height, uint16_t width, char* s1, cha
 		for (int i = 0; i < width + 1; i++)
 		{
 			printf("---+");
+		}
+		printf("\n");
+	}
+}
+
+__host__ void PrintX(uint16_t* X, uint16_t height, uint16_t width, char* s2)
+{
+	printf("  ");
+	for (int i = 0; i < width; i++)
+	{
+		if (i == 0)
+			printf("  ");
+		else
+			printf("%2c", s2[i - 1]);
+	}
+	printf("\n");
+
+	for (int i = 0; i < height; i++)
+	{
+		printf("%2c", ALPHSTART + i);
+
+		for (int j = 0; j < width; j++)
+		{
+			printf("%2d", X[i + ALPHLEN * j]);
 		}
 		printf("\n");
 	}
@@ -321,59 +369,70 @@ void freeList(Node* head)
 	}
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int* c, const int* a, const int* b, unsigned int size)
+// Helper function for using CUDA
+cudaError_t LevenshteinGPU(char* s1, char* s2, const uint16_t s1Len, const uint16_t s2Len, uint16_t* D, uint16_t* X)
 {
-	int* dev_a = 0;
-	int* dev_b = 0;
-	int* dev_c = 0;
+	uint16_t* d_X;
+	uint16_t* d_D;
+	char* d_s1, * d_s2;
 	cudaError_t cudaStatus;
 
 	// Choose which GPU to run on, change this on a multi-GPU system.
 	cudaStatus = cudaSetDevice(0);
-	if (cudaStatus != cudaSuccess) {
+	if (cudaStatus != cudaSuccess)
+	{
 		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
 		goto Error;
 	}
 
-	// Allocate GPU buffers for three vectors (two input, one output)    .
-	cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed!");
+	cudaStatus = cudaMalloc(&d_X, (s2Len + 1) * ALPHLEN * sizeof(uint16_t));
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "d_X cudaMalloc failed!");
 		goto Error;
 	}
 
-	cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed!");
+	cudaStatus = cudaMalloc(&d_D, (s1Len + 1) * (s2Len + 1) * sizeof(uint16_t));
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "d_D cudaMalloc failed!");
 		goto Error;
 	}
 
-	cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed!");
+	cudaStatus = cudaMalloc(&d_s1, s1Len * sizeof(char));
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "d_s1 cudaMalloc failed!");
 		goto Error;
 	}
 
-	// Copy input vectors from host memory to GPU buffers.
-	cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
+	cudaStatus = cudaMalloc(&d_s2, s2Len * sizeof(char));
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "d_s2 cudaMalloc failed!");
 		goto Error;
 	}
 
-	cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
+	cudaStatus = cudaMemcpy(d_s1, s1, s1Len * sizeof(char), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "s1 cudaMemcpy failed!");
 		goto Error;
 	}
 
-	// Launch a kernel on the GPU with one thread for each element.
-	addKernel << <1, size >> > (dev_c, dev_a, dev_b);
+	cudaStatus = cudaMemcpy(d_s2, s2, s2Len * sizeof(char), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "s2 cudaMemcpy failed!");
+		goto Error;
+	}
+
+	calculateX<<<1, 32 >>>(d_X, d_s2, s2Len);
 
 	// Check for any errors launching the kernel
 	cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
+	if (cudaStatus != cudaSuccess)
+	{
 		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
 		goto Error;
 	}
@@ -381,22 +440,58 @@ cudaError_t addWithCuda(int* c, const int* a, const int* b, unsigned int size)
 	// cudaDeviceSynchronize waits for the kernel to finish, and returns
 	// any errors encountered during the launch.
 	cudaStatus = cudaDeviceSynchronize();
-	if (cudaStatus != cudaSuccess) {
+	if (cudaStatus != cudaSuccess)
+	{
 		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
 		goto Error;
 	}
 
+	cudaStatus = cudaMemcpy(X, d_X, (s2Len + 1) * ALPHLEN * sizeof(uint16_t), cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "X cudaMemcpy failed!");
+		goto Error;
+	}
+
 	// Copy output vector from GPU buffer to host memory.
-	cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
+	cudaStatus = cudaMemcpy(D, d_D, (s1Len + 1) * (s2Len + 1) * sizeof(uint16_t), cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "D cudaMemcpy failed!");
 		goto Error;
 	}
 
 Error:
-	cudaFree(dev_c);
-	cudaFree(dev_a);
-	cudaFree(dev_b);
+	cudaFree(d_D);
+	cudaFree(d_X);
+	cudaFree(d_s1);
+	cudaFree(d_s2);
 
 	return cudaStatus;
+}
+
+__global__ void calculateX(uint16_t* X, char* s2, const uint16_t s2Len)
+{
+	__shared__ uint16_t buffer[ALPHLEN];
+
+	if (threadIdx.x < ALPHLEN)
+	{
+		uint16_t prev = 0;
+		uint16_t num = 0;
+		for (int i = 0; i < s2Len + 1; i++)
+		{
+			if (i == 0)
+				num = 0;
+			else if (s2[i - 1] == ALPHSTART + threadIdx.x)
+				num = i;
+			else
+				num = prev;
+
+			buffer[threadIdx.x] = num;
+			prev = num;
+
+			if (threadIdx.x == 0)
+				memcpy(X + ALPHLEN * i, buffer, ALPHLEN * sizeof(uint16_t));
+		}
+	}
 }
